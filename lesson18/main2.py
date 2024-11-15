adac3c88080fa4f548f45ea6d5156d40a3d320a4
BlinVis <blin_visoka@hotmail.com>
2024-11-15 14:03:44 +0100

    Update main2.py

--- lesson18/main2.py
+++ lesson18/main2.py
@@ -15,11 +15,12 @@ avg_price=books_df['Price'].mean()
 
 coll1,coll2,coll3,coll4=st.columns(4)
 
-coll1.metric('tt',total_books)
-coll2.metric('tt',unique_title)
+coll1.metric('total books',total_books)
+coll2.metric('number of unique titles',unique_title)
 
-coll3.metric('tt',avg_rating)
-coll4.metric('tt',avg_price)
+coll3.metric('average rating',avg_rating)
+coll4.metric('average price in dollars',avg_price)
+
 
 col1,col2=st.columns(2)
 
@@ -35,5 +36,25 @@ with col2:
 st.subheader('Genre distribution')
 fig=px.pie(books_df,names='Genre',title='Most liked genres',color='Genre',color_discrete_sequence=px.colors.sequential.Plasma)
 st.plotly_chart(fig)
+max_rating = books_df['User Rating'].max()
 
+# Filter the dataset to get the book(s) with the maximum rating
+best_rated_books = books_df[books_df['User Rating'] == max_rating]
+top10_best_rated_books=best_rated_books.drop_duplicates(subset='Name').head(10)
+
+
+
+# Display the result
+fig = px.bar(
+    top10_best_rated_books,
+    x='Name',
+    y='User Rating',
+    color='User Rating',
+    title='Best Rated Unique Books',
+    labels={'Name': 'Book Title', 'User Rating': 'Rating'},
+    color_continuous_scale='Viridis'
+)
+
+# Show the plot in the Streamlit app
+st.plotly_chart(fig)
 
