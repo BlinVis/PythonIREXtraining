6d0460fe4a72f6196d3b0d732fab4755146d2c9b
BlinVis <blin_visoka@hotmail.com>
2024-11-29 07:18:33 -0800

    ss

--- OnlineRecipeBook/routers/category.py
+++ OnlineRecipeBook/routers/category.py
@@ -2,6 +2,9 @@ import sqlite3
 
 from typing import List
 
+from OnlineRecipeBook.models.category import CategoryCreate
+from lesson11.main import current_time
+from lesson23.connect import response
 from streamlit import status
 
 from models.category import CategoryBase, Category
@@ -39,4 +42,55 @@ def get_categories():
     return category_list
 
 
+@router.post("/categories/", response_model=category)
+def create_category(category: CategoryCreate):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    try:
+        cursor.execute("inset into categories (name) values (?)", (category.name))
+        conn.commit()
+        category_id = cursor.lastrowid
+        return category(id=category_id, name=category.name)
+    except sqlite3.IntegrityError:
+        conn.close()
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLIT,
+            details=f"The category {category.name} already exists"
+        )
+
+    except Exception as e:
+        conn.close()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f'An error has occurred : {e}'
+
+        )
+    finally:
+        conn.close()
+
+@router.put("/categories/{category_id}",response_model=Category)
+def update_category(category_id:int,category: CategoryCreate):
+    conn=get_db_connection()
+    cursor=conn.cursor()
+
+    cursor.execute("Update categories set name=? where id=?",(category.name,category_id))
+    if cursor.rowcount==0:
+        conn.close()
+        raise HTTPException(status_code=404,detail="Category not found")
+
+    conn.commit()
+    conn.close()
+    return Category(id=category_id,name=category.name)
+@router.delete("/categories/{category_id}",response_model=dict)
+def delete_category(category:int):
+    conn=get_db_connection
+    cursor=conn.cursor()
+    cursor.execute('Delete from categories where id=?', (category_id))
+    if cursor.rowcount==0:
+        conn.close()
+        raise  HTTPException(status_code=404,detail="Category not found")
+
+    conn.commit()
+    conn.close()
+    return {"details":"Category deleted"}
 
