53ae121a41f7312535713e8fa6dc138e0a2b7805
BlinVis <blin_visoka@hotmail.com>
2024-11-29 06:21:52 -0800

    ww

--- OnlineRecipeBook/routers/category.py
+++ OnlineRecipeBook/routers/category.py
@@ -1,12 +1,42 @@
 import sqlite3
-from typing import  List
+
+from typing import List
+
 from streamlit import status
+
 from models.category import CategoryBase, Category
+
 from database import get_db_connection
+
 from fastapi import APIRouter, HTTPException
-router=APIRouter()
-@router.get('/categories/',List[Category])
+
+
+
+
+
+router = APIRouter()
+
+
+
+
+
+@router.get("/categories/", response_model=List[Category])
+
 def get_categories():
-    conn=get_db_connection()
-    cursor=conn.cursor()
+
+    conn = get_db_connection()
+
+    cursor = conn.cursor()
+
+    cursor.execute("Select if, name from categories")
+
+    categories = cursor.fetchall()
+
+    conn.close()
+
+    category_list = [{"id": cat[0], "name": cat[1]} for cat in categories]
+
+    return category_list
+
+
 
